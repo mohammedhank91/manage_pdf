a239557af9b92baddae6cdf368165ee14d9bc52d
mohammed chaaraoui <shoshomedo876@gmail.com>
2025-04-29 15:21:36 +0100

    Adds installer upload to file.io
    Implements a new workflow step to upload the generated installer to file.io, providing a download link. The link is then saved as a GitHub Environment Variable and output for subsequent steps.
    This allows easy distribution of the installer. The link expires after 14 days or the first download.

--- run_app.py
+++ run_app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import os
 import sys
-# Add project root to sys.path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-# Import and run the main application
 from src.pdf_manage import PdfManager, QApplication
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
